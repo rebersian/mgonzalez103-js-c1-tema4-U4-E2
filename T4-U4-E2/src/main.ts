ca3486d52c6123743ddc19733a46437d744c84ee
Maria <rebersian@gmail.com>
2026-05-13 22:21:14 +0200

    ini

--- T4-U4-E2/src/main.ts
+++ T4-U4-E2/src/main.ts
@@ -1,8 +1,13 @@
-import './assets/main.css'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/main.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
